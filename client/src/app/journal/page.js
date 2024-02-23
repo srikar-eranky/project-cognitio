328c4cbd7ba5a53d648e02e2a9d38dce47ea1b91
lavinma <lavin.ma18@gmail.com>
2024-02-23 12:37:44 -0800

    Journal update
    Dating on the journal page

--- client/src/app/journal/page.js
+++ client/src/app/journal/page.js
@@ -1,8 +1,57 @@
 import styles from './page.module.css'
 
 function JournalGreeting(){
+    let newDate = new Date()
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+
+    switch (month) {
+        case 1:
+            month = "January";
+            break;
+        case 2:
+            month = "February";
+            break;
+        case 3:
+            month = "March";
+            break;
+        case 4:
+            month = "April";
+            break;
+        case 5:
+            month = "May";
+            break;
+        case 6:
+            month = "June";
+            break;
+        case 7:
+            month = "July";
+            break;
+        case 8:
+            month = "August";
+            break;
+        case 8:
+            month = "September";
+            break;
+        case 10:
+            month = "October";
+            break;
+        case 11:
+            month = "November";
+            break;
+        case 12:
+            month = "December";
+    }
+
     return (
-        <h1 className={styles.title}>Journal!</h1>
+        <div>
+            <h1 className={styles.title}>Journal!</h1>
+            <h1 className={styles.title}>{month} {date}, {year}</h1>
+            <h1 className={styles.title}>Prompt: </h1>
+        </div>
+       
+       
     )
 }
 
