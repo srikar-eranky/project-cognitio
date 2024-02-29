a2960f505ddf5a8e5ba057eb72c87709d2de35a1
Jamal Karim <1jamalkarim@gmail.com>
2024-02-28 23:44:41 -0800

    add final styling for emojis

--- client/src/app/components/Emoji/emoji.js
+++ client/src/app/components/Emoji/emoji.js
@@ -1,46 +1,62 @@
-import styles from './emoji.module.css'
-import Image from 'next/image';
-import rating1 from './/ratingFaces/rating1.svg'
-import rating2 from './/ratingFaces/rating2.svg'
+import styles from "./emoji.module.css";
+import Image from "next/image";
+import rating1 from ".//ratingFaces/rating1.svg";
+import rating2 from ".//ratingFaces/rating2.svg";
+import rating3 from ".//ratingFaces/rating3.svg";
+import rating4 from ".//ratingFaces/rating4.svg";
+import rating5 from ".//ratingFaces/rating5.svg";
+import rating6 from ".//ratingFaces/rating6.svg";
+import rating7 from ".//ratingFaces/rating7.svg";
+import rating8 from ".//ratingFaces/rating8.svg";
+import rating9 from ".//ratingFaces/rating9.svg";
+import rating10 from ".//ratingFaces/rating10.svg";
 
 const faces = [
-    styles.face1,
-    styles.face2
-]
+  styles.face1,
+  styles.face2,
+  styles.face3,
+  styles.face4,
+  styles.face5,
+  styles.face6,
+  styles.face7,
+  styles.face8,
+  styles.face9,
+  styles.face10,
+];
 
-function Emoji({rating}){
-    const getRatingSrc = () => {
-        switch(rating) {
-            case 1:
-                return rating1;
-            case 2:
-                return rating2;
-        }
-    };
+function Emoji({ rating }) {
+  const getRatingSrc = () => {
+    switch (rating) {
+      case 1:
+        return rating1;
+      case 2:
+        return rating2;
+      case 3:
+        return rating3;
+      case 4:
+        return rating4;
+      case 5:
+        return rating5;
+      case 6:
+        return rating6;
+      case 7:
+        return rating7;
+      case 8:
+        return rating8;
+      case 9:
+        return rating9;
+      case 10:
+        return rating10;
+    }
+  };
 
-    const faceStyle = faces[rating - 1]
+  const faceStyle = faces[rating - 1];
 
-    return (
-        <div className={styles.circle}>
-            <Image
-                src={getRatingSrc()}
-                alt='Rating face'
-                className={faceStyle}
-            />
-        </div>
-    )
+  return (
+    <div className={styles.circle}>
+      <Image src={getRatingSrc()} alt="Rating face" className={faceStyle} />
+    </div>
+  );
 }
 
 export default Emoji;
-
-// function Emoji(){
-//     return (
-//         <div className={styles.circle}>
-//             <Image
-//                 src={rating1}
-//                 alt='Rating face'
-//                 className={styles.face1}
-//             />
-//         </div>
-//     )
-// }
--- client/src/app/rating/page.js
+++ client/src/app/rating/page.js
@@ -1,15 +1,62 @@
-import styles from './page.module.css'
-import Emoji from '../components/Emoji/emoji';
+import styles from "./page.module.css";
+import Emoji from "../components/Emoji/emoji";
 
-function RatingGreeting(){
-    return (
-        <>
+function RatingGreeting() {
+  return (
+    <>
+      <div className={styles.titleContainer}>
         <h1 className={styles.title}>How are you feeling today?</h1>
-        <Emoji rating={1} />
-        <Emoji rating={2} />
-        <Emoji rating={1} />
-        </>
-    )
+      </div>
+
+      <div className={styles.ratingContainer}>
+        <div className={styles.emojiRow}>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={1} />
+            <span>1</span>
+          </div>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={2} />
+            <span>2</span>
+          </div>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={3} />
+            <span>3</span>
+          </div>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={4} />
+            <span>4</span>
+          </div>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={5} />
+            <span>5</span>
+          </div>
+        </div>
+
+        <div className={styles.emojiRow}>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={6} />
+            <span>6</span>
+          </div>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={7} />
+            <span>7</span>
+          </div>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={8} />
+            <span>8</span>
+          </div>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={9} />
+            <span>9</span>
+          </div>
+          <div className={styles.emojiContainer}>
+            <Emoji rating={10} />
+            <span>10</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RatingGreeting;
