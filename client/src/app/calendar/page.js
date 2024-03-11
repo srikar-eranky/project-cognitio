e8e87c64885f0726deefe6cfc79552e254b493ff
Jamal Karim <1jamalkarim@gmail.com>
2024-03-11 08:02:03 -0700

    add css reset and start calendar ui

--- client/src/app/calendar/page.js
+++ client/src/app/calendar/page.js
@@ -1,9 +1,28 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
-function CalendarGreeting(){
-    return (
-        <h1 className={styles.title}>Calendar!</h1>
-    )
+function CreateCalendarDisplay() {
+  const days = Array.from({ length: 35 }, (_, i) => i + 1);
+
+  return (
+    <>
+      <style js="true" global="true">
+        {`
+      body {
+          background: linear-gradient(12.8deg, #C5EFCB -29.07%, #5B7F77 94.75%);
+       }
+        `}
+      </style>
+
+      <h1 className={styles.title}>Calendar!</h1>
+      <div className={styles.container}>
+        {days.map((day) => (
+          <div key={day} className={styles.day}>
+            {day}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default CalendarGreeting;
+export default CreateCalendarDisplay;
