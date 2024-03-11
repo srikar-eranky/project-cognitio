ff51aa010789d534c668ca096c7a4964591db852
Jamal Karim <1jamalkarim@gmail.com>
2024-03-11 11:55:34 -0700

    add final styling to calendar page

--- client/src/app/calendar/page.js
+++ client/src/app/calendar/page.js
@@ -15,14 +15,17 @@ function CreateCalendarDisplay() {
 
       <h1 className={styles.title}>Calendar</h1>
       <div className={styles.weekDaysContainer}>
-        <p className={`${styles.weekDay} ${styles.sunday}`}>Sun</p>
-        <p className={`${styles.weekDay} ${styles.monday}`}>Mon</p>
-        <p className={`${styles.weekDay} ${styles.tuesday}`}>Tue</p>
-        <p className={`${styles.weekDay} ${styles.wednesday}`}>Wed</p>
-        <p className={`${styles.weekDay} ${styles.thursday}`}>Thu</p>
-        <p className={`${styles.weekDay} ${styles.friday}`}>Fri</p>
-        <p className={`${styles.weekDay} ${styles.saturday}`}>Sat</p>
+        <div className={styles.weekDays}>
+          <p className={`${styles.weekDay} ${styles.sunday}`}>Sun</p>
+          <p className={`${styles.weekDay} ${styles.monday}`}>Mon</p>
+          <p className={`${styles.weekDay} ${styles.tuesday}`}>Tue</p>
+          <p className={`${styles.weekDay} ${styles.wednesday}`}>Wed</p>
+          <p className={`${styles.weekDay} ${styles.thursday}`}>Thu</p>
+          <p className={`${styles.weekDay} ${styles.friday}`}>Fri</p>
+          <p className={`${styles.weekDay} ${styles.saturday}`}>Sat</p>
+        </div>
       </div>
+
       <div className={styles.container}>
         {days.map((day) => (
           <div key={day} className={styles.day}>
