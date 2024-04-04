c4ef955acf6aaf9daa68df620a5247f57f6147a0
Samuel Eun <samuel9eun@gmail.com>
2024-04-03 18:52:28 -0700

    restart backend

--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  googleId: { type: String, required: true, unique: true },
-  displayName: { type: String },
-  email: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model('User', userSchema);
